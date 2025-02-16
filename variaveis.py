4cee2baff6361473718c6f9259404914b19670be
Sabrinafogliene <sabrina.castilho4@gmail.com>
2025-02-16 18:47:08 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,20 @@
+nome = "Carlos"
+idade = 30
+altura = 1.80
+eh_estudante = True
+
+#Imprimindo os novos valores das variáveis
+print("Nome atualizado: ", nome)
+print("Idade atualizada: ", idade)
+print("Altura atualizada: ", altura)
+print("É estudante? (atualizado)", eh_estudante)
+
+#Receber um número do usuário
+mensagem = input("Digite uma mensagem: ")
+
+#Imprimir a mensagem original
+print("Você digitou: ", {mensagem})
+
+#Converter a mensagem para a caixa alta e imprimir
+mensagem_caixa_alta = mensagem.upper()
+print ("Mensagem em caixa alta:", {mensagem_caixa_alta}  )
